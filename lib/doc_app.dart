997611f86616ce090899ebbc22e1a2a8797a9933
shroida <shroida012@gmail.com>
2025-01-25 06:34:31 +0200

    add more files to structres

---
+++ lib/doc_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DocApp extends StatelessWidget {
+const DocApp({ super.key });
+
+  @override
+  Widget build(BuildContext context){
+    return Placeholder();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,6 @@
+import 'package:doc_on_time/doc_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({
-    super.key,
-  });
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('coco'),
-        ),
-        body: const Center(child: Text('Hellp')));
-  }
+  runApp(const DocApp());
 }
