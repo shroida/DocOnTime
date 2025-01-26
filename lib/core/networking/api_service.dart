50b108c8c8d62f7ff013b30c45e20bd2120989d2
shroida <shroida012@gmail.com>
2025-01-26 06:24:12 +0200

    create request body for login and signup and generate their files

--- lib/core/networking/api_service.dart
+++ lib/core/networking/api_service.dart
@@ -1,9 +1,23 @@
 import 'package:dio/dio.dart';
 import 'package:doc_on_time/core/networking/api_constants.dart';
+import 'package:doc_on_time/featuers/login/data/models/login_request_body.dart';
+import 'package:doc_on_time/featuers/login/data/models/login_response.dart';
+import 'package:doc_on_time/featuers/signup/data/models/sign_up_request_body.dart';
+import 'package:doc_on_time/featuers/signup/data/models/sign_up_response.dart';
 import 'package:retrofit/http.dart';
 part 'api_service.g.dart';
 
 @RestApi(baseUrl: ApiConstants.apiBaseUrl)
 abstract class ApiService {
   factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
+
+  @POST(ApiConstants.login)
+  Future<LoginResponse> login(
+    @Body() LoginRequestBody loginRequestBody,
+  );
+
+  @POST(ApiConstants.signup)
+  Future<SignupResponse> signup(
+    @Body() SignupRequestBody signupRequestBody,
+  );
 }
