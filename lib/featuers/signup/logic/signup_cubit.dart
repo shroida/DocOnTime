ad355f1847b52b8a3385cfeccee19ead9acab5a9
shroida <shroida012@gmail.com>
2025-01-27 07:29:29 +0200

    sign up cubit

---
+++ lib/featuers/signup/logic/signup_cubit.dart
@@ -0,0 +1,38 @@
+import 'package:doc_on_time/featuers/signup/data/repos/sign_up_rep.dart';
+import 'package:doc_on_time/featuers/signup/logic/signup_state.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../data/models/sign_up_request_body.dart';
+
+class SignupCubit extends Cubit<SignupState> {
+  final SignupRepo _signupRepo;
+  SignupCubit(this._signupRepo) : super(const SignupState.initial());
+
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController passwordConfirmationController =
+      TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  void emitSignupStates() async {
+    emit(const SignupState.signupLoading());
+    final response = await _signupRepo.signup(
+      SignupRequestBody(
+        name: nameController.text,
+        email: emailController.text,
+        phone: phoneController.text,
+        password: passwordController.text,
+        passwordConfirmation: passwordConfirmationController.text,
+        gender: 0,
+      ),
+    );
+    response.when(success: (signupResponse) {
+      emit(SignupState.signupSucess(signupResponse));
+    }, failure: (error) {
+      emit(SignupState.signupError(error: error.apiErrorModel.message ?? ''));
+    });
+  }
+}
