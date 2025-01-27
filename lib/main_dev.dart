4994cdcdafae429fa0f4b5d49139a7f2590fd0c5
shroida <shroida012@gmail.com>
2025-01-27 12:44:50 +0200

    generate options firebase

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -2,13 +2,21 @@ import 'package:doc_on_time/core/di/dependency_injection.dart';
 import 'package:doc_on_time/core/routing/app_router.dart';
 import 'package:doc_on_time/doc_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main()async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+
   // Set up dependency injection
   setUpGetIt();
-  
+  // fixing hiddin texts in dev version
+  await ScreenUtil.ensureScreenSize();
+
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.amber,
+    systemNavigationBarColor: Colors.red,
+  ));
   // Initialize app router
   final appRouter = AppRouter();
 
