a4dc1f8f9893b69f5ace82fe6971c81e46d75f88
shroida <shroida012@gmail.com>
2025-01-28 11:02:57 +0200

    home api service

---
+++ lib/featuers/home/data/apis/home_api_service.dart
@@ -0,0 +1,16 @@
+import 'package:dio/dio.dart';
+import 'package:doc_on_time/core/networking/api_constants.dart';
+import 'package:doc_on_time/featuers/home/data/apis/home_api_constatns.dart';
+import 'package:doc_on_time/featuers/home/data/models/specializations_response_model.dart';
+import 'package:retrofit/http.dart';
+part 'home_api_service.g.dart';
+
+@RestApi(baseUrl: ApiConstants.apiBaseUrl )
+
+abstract class HomeApiService {
+ factory HomeApiService(Dio dio) = _HomeApiService;
+
+ @GET(HomeApiConstants.specializationEP) 
+  Future<SpecializationsResponseModel> getHomeData();
+
+}
--- lib/featuers/home/data/models/specializations_response_model.g.dart
+++ lib/featuers/home/data/models/specializations_response_model.g.dart
@@ -16,6 +16,12 @@ SpecializationsResponseModel _$SpecializationsResponseModelFromJson(
           .toList(),
     );
 
+Map<String, dynamic> _$SpecializationsResponseModelToJson(
+        SpecializationsResponseModel instance) =>
+    <String, dynamic>{
+      'data': instance.specializationDataList,
+    };
+
 SpecializationsData _$SpecializationsDataFromJson(Map<String, dynamic> json) =>
     SpecializationsData(
       id: (json['id'] as num?)?.toInt(),
@@ -26,6 +32,13 @@ SpecializationsData _$SpecializationsDataFromJson(Map<String, dynamic> json) =>
           .toList(),
     );
 
+Map<String, dynamic> _$SpecializationsDataToJson(
+        SpecializationsData instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'doctors': instance.doctorsList,
+    };
 
 Doctors _$DoctorsFromJson(Map<String, dynamic> json) => Doctors(
       id: (json['id'] as num?)?.toInt(),
@@ -38,3 +51,13 @@ Doctors _$DoctorsFromJson(Map<String, dynamic> json) => Doctors(
       degree: json['degree'] as String,
     );
 
+Map<String, dynamic> _$DoctorsToJson(Doctors instance) => <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'email': instance.email,
+      'phone': instance.phone,
+      'photo': instance.photo,
+      'gender': instance.gender,
+      'appoint_price': instance.price,
+      'degree': instance.degree,
+    };
