e405a7ea63d25f3a8f33d0711df0c5b4fc6341f8
shroida <shroida012@gmail.com>
2025-01-28 07:51:41 +0200

    DoctorsListView done

---
+++ lib/featuers/home/ui/widgets/doctors_list/doctors_list_view.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import '../../../data/models/specializations_response_model.dart';
+
+class DoctorsListView extends StatelessWidget {
+  final List<Doctors?>? doctorsList;
+  const DoctorsListView({super.key, this.doctorsList});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+        itemCount: doctorsList?.length,
+        itemBuilder: (context, index) {
+          return DoctorsListViewItem(
+            doctorsModel: doctorsList?[index],
+          );
+        },
+      ),
+    );
+  }
+}
