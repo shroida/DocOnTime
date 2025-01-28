bdf11ccf76fe84e03ce0f09ba1161ac4c53c97b2
shroida <shroida012@gmail.com>
2025-01-28 06:37:54 +0200

    make home screen ready

--- lib/featuers/home/ui/home_screen.dart
+++ lib/featuers/home/ui/home_screen.dart
@@ -5,6 +5,20 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('HomeScreen');
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Container(
+              width: double.infinity,
+              color: Colors.red[400],
+              margin: EdgeInsets.fromLTRB(20, 16, 20, 28),
+
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  HomeTopBar()
+                ],
+              )),
+        ));
   }
 }
